c2942e18a73122fd99ecb616f5736dbc569fd4d2
Fernando <fernando.jvalle@hotmail.com>
2019-11-12 20:36:55 -0300

    Resolve bug

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use('/cart', cartRouter);
 app.use('/admin/posts', /*[verifyAuth],*/  adminPostsRouter);
 app.use('/admin/products', [verifyAuth], adminProductsRouter);
 app.use('/admin/switches', [verifyAuth], switchRouter);
+app.use('/admin/sensores', [verifyAuth], sensoresRouter);
 app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
